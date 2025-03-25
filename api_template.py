c516a8191b24606b5c6afc5cba33d0ce5afd0f2e
Aengus Roberts <asr66@bath.ac.uk>
2025-03-25 12:40:09 +0000

    Updated api_template.py

--- api_template.py
+++ api_template.py
@@ -2,9 +2,11 @@ from sentinelhub import SHConfig
 
 config = SHConfig()
 
-config.instance_id = "AAA"  # Replace with your actual instance ID
 config.sh_client_id = "AAA"  # Replace with your actual client ID
 config.sh_client_secret = "AAA"  # Replace with your actual client secret
+config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
+config.sh_base_url = "https://sh.dataspace.copernicus.eu"
+config.save("cdse")
 
 if not config.sh_client_id or not config.sh_client_secret:
     raise ValueError("Please provide Sentinel Hub client credentials!")
